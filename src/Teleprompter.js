3925031476b2a520d8c14100c3f2633e55a56cf1
Elijah Manor <elijah.manor@gmail.com>
2020-05-30 11:24:26 -0500

    Added shadows and height

--- src/Teleprompter.js
+++ src/Teleprompter.js
@@ -4,11 +4,24 @@ import styled from 'styled-components'
 const StyledTeleprompter = styled.div`
   font-size: 5.25rem;
   width: 100%;
-  height: 200px;
+  height: 24rem;
   overflow: scroll;
   scroll-behavior: smooth;
   display: block;
   margin-bottom: 1rem;
+  background: transparent no-repeat;
+  background-image: radial-gradient(
+      farthest-side at 50% 0,
+      rgba(0, 0, 0, 0.1),
+      rgba(0, 0, 0, 0)
+    ),
+    radial-gradient(
+      farthest-side at 50% 100%,
+      rgba(0, 0, 0, 0.1),
+      rgba(0, 0, 0, 0)
+    );
+  background-position: 0 0, 0 100%;
+  background-size: 100% 14px;
 `
 
 const Interim = styled.div`
