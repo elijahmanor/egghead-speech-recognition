ea9b668f44cdbd0023488ed94e56eb14518677ea
Elijah Manor <elijah.manor@gmail.com>
2020-05-30 13:06:39 -0500

    Sat May 30 13:06:39 CDT 2020

---
+++ playlist.md
@@ -0,0 +1,7 @@
+
+## Teleprompter
+
+[https://github.com/Elijah Manor/egghead-speech-recognition/tree/teleprompter](https://github.com/Elijah Manor/egghead-speech-recognition/tree/teleprompter)
+
+[https://codesandbox.io/embed/github/Elijah Manor/egghead-speech-recognition/tree/teleprompter?module=/src/index.js](https://codesandbox.io/embed/github/Elijah Manor/egghead-speech-recognition/tree/teleprompter?module=/src/index.js)
+
--- src/Teleprompter.js
+++ src/Teleprompter.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import stringSimilarity from 'string-similarity'
 
 const StyledTeleprompter = styled.div`
   font-size: 5.25rem;
@@ -33,23 +34,132 @@ const Interim = styled.div`
   display: inline-block;
 `
 
+const cleanWord = word =>
+  word
+    .trim()
+    .toLocaleLowerCase()
+    .replace(/[^a-z]/gi, '')
+
 export default function Teleprompter({
   words,
   progress,
   listening,
   onChange
 }) {
+  const recog = React.useRef(null)
+  const scrollRef = React.useRef(
+    null
+  )
+  const [
+    results,
+    setResults
+  ] = React.useState('')
+
+  React.useEffect(() => {
+    const SpeechRecognition =
+      window.SpeechRecognition ||
+      window.webkitSpeechRecognition
+    recog.current = new SpeechRecognition()
+    recog.current.continuous = true
+    recog.current.interimResults = true
+  }, [])
+
+  React.useEffect(() => {
+    if (listening) {
+      recog.current.start()
+    } else {
+      recog.current.stop()
+    }
+  }, [listening])
+
+  React.useEffect(() => {
+    const handleResult = ({
+      results
+    }) => {
+      const interim = Array.from(
+        results
+      )
+        .filter(r => !r.isFinal)
+        .map(r => r[0].transcript)
+        .join(' ')
+      setResults(interim)
+
+      const newIndex = interim
+        .split(' ')
+        .reduce((memo, word) => {
+          if (
+            memo >= words.length
+          ) {
+            return memo
+          }
+          const similarity = stringSimilarity.compareTwoStrings(
+            cleanWord(word),
+            cleanWord(words[memo])
+          )
+          memo +=
+            similarity > 0.75
+              ? 1
+              : 0
+          return memo
+        }, progress)
+      if (
+        newIndex > progress &&
+        newIndex <= words.length
+      ) {
+        onChange(newIndex)
+      }
+    }
+    recog.current.addEventListener(
+      'result',
+      handleResult
+    )
+    return () => {
+      recog.current.removeEventListener(
+        'result',
+        handleResult
+      )
+    }
+  }, [onChange, progress, words])
+
+  React.useEffect(() => {
+    /* eslint-disable no-unused-expressions */
+    scrollRef.current
+      .querySelector(
+        `[data-index='${
+          progress + 3
+        }']`
+      )
+      ?.scrollIntoViewIfNeeded({
+        behavior: 'smooth',
+        block: 'start'
+      })
+  }, [progress])
+
   return (
     <React.Fragment>
-      <StyledTeleprompter>
+      <StyledTeleprompter
+        ref={scrollRef}
+      >
         {words.map((word, i) => (
           <span
             key={`${word}:${i}`}
+            data-index={i}
+            style={{
+              color:
+                i < progress
+                  ? '#ccc'
+                  : '#000'
+            }}
           >
             {word}{' '}
           </span>
         ))}
       </StyledTeleprompter>
+      {results && (
+        <Interim>
+          {results}
+        </Interim>
+      )}
     </React.Fragment>
   )
 }
